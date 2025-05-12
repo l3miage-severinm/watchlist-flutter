a7cb4a70b47cfa7980427b4e53a6cf9f9dd78fcb
l3miage-severinm <matteo.severini@etu.univ-grenoble-alpes.fr>
2025-05-12 15:46:32 +0200

    feat: simple frontend to show a dropdown list (with fake data) disabled when not connected and enabled when connected

--- lib/screens/MovieDetailsScreen.dart
+++ lib/screens/MovieDetailsScreen.dart
@@ -1,24 +1,46 @@
 import 'package:flutter/material.dart';
 import '../models/Movie.dart';
+import '../services/AuthService.dart';
 
-class MovieDetailsScreen extends StatelessWidget {
+class MovieDetailsScreen extends StatefulWidget {
   final Movie movie;
 
   const MovieDetailsScreen({super.key, required this.movie});
 
   @override
+  _MovieDetailsScreenState createState() => _MovieDetailsScreenState();
+}
+
+class _MovieDetailsScreenState extends State<MovieDetailsScreen> {
+  bool isAuthenticated = false;
+  List<String> _userLists = ["Ma liste 1", "Ma liste 2", "Mes favoris"];
+
+  @override
+  void initState() {
+    super.initState();
+    checkAuthStatus();
+  }
+
+  Future<void> checkAuthStatus() async {
+    final authStatus = await AuthService.isAuthenticated();
+    setState(() {
+      isAuthenticated = authStatus;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(movie.title)),
+      appBar: AppBar(title: Text(widget.movie.title)),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Center(
-              child: movie.posterPath.isNotEmpty
+              child: widget.movie.posterPath.isNotEmpty
                   ? Image.network(
-                "https://image.tmdb.org/t/p/w500${movie.posterPath}",
+                "https://image.tmdb.org/t/p/w500${widget.movie.posterPath}",
                 height: 300,
                 fit: BoxFit.cover,
               )
@@ -26,20 +48,44 @@ class MovieDetailsScreen extends StatelessWidget {
             ),
             const SizedBox(height: 16),
             Text(
-              "Titre: ${movie.title}",
+              "Titre: ${widget.movie.title}",
               style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 8),
-            Text("Sortie: ${movie.releaseDate}"),
+            Text("Sortie: ${widget.movie.releaseDate}"),
             const SizedBox(height: 8),
-            Text("Note: ${movie.voteAverage}/10"),
+            Text("Note: ${widget.movie.voteAverage}/10"),
             const SizedBox(height: 16),
             const Text(
               "Synopsis:",
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 8),
-            Text(movie.overview, textAlign: TextAlign.justify),
+            Text(widget.movie.overview, textAlign: TextAlign.justify),
+            const SizedBox(height: 20),
+            DropdownButtonHideUnderline(
+              child: ButtonTheme(
+                alignedDropdown: true,
+                child: DropdownButton<String>(
+                  value: null,
+                  hint: const Text("Ajouter à une liste"),
+                  disabledHint: const Text("Ajouter à une liste"),
+                  items: isAuthenticated
+                      ? _userLists.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList()
+                      : null,
+                  onChanged: (String? newValue) {
+                    if (newValue != null) {
+                      print("Ajouter à $newValue");
+                    }
+                  },
+                ),
+              ),
+            ),
           ],
         ),
       ),

6f3df4e4ed7096b083f5f90d4f9766ad1c13ab05
l3miage-severinm <matteo.severini@etu.univ-grenoble-alpes.fr>
2025-05-14 12:56:02 +0200

    ref: add TODOs

--- lib/services/AuthService.dart
+++ lib/services/AuthService.dart
@@ -4,7 +4,9 @@ import 'package:http/http.dart' as http;
 import 'package:localstorage/localstorage.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+// TODO : use a singleton
 class AuthService {
+  // TODO : centralize constants to avoid redundancy
   static const String _apiKey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYzRmOGVlNjUwMWFjMjlkZGFhNmE2OTBlNjJhMmE5NiIsIm5iZiI6MTc0MjM4MzY3MC44ODMwMDAxLCJzdWIiOiI2N2RhYWEzNmU4MzAyNTMzMjA2Y2FlOTgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.EFzi4KselXU_YFtIW3SyRcGmQLKSHehHS-XIVdSkODs";
   static const String _accessTokenUrl = "https://api.themoviedb.org/4/auth/access_token";
   static const String _requestTokenUrl = "https://api.themoviedb.org/4/auth/request_token";
@@ -22,6 +24,7 @@ class AuthService {
   }
 
   static Future<void> authenticate() async {
+    // TODO : use a middleware to set the headers
     final response = await http.post(
       Uri.parse(_requestTokenUrl),
       headers: {
--- lib/services/MovieService.dart
+++ lib/services/MovieService.dart
@@ -3,12 +3,15 @@ import 'package:http/http.dart' as http;
 import '../models/Movie.dart';
 
 class MovieService {
+  // TODO : centralize constants to avoid redundancy
   static const String _token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYzRmOGVlNjUwMWFjMjlkZGFhNmE2OTBlNjJhMmE5NiIsIm5iZiI6MTc0MjM4MzY3MC44ODMwMDAxLCJzdWIiOiI2N2RhYWEzNmU4MzAyNTMzMjA2Y2FlOTgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.EFzi4KselXU_YFtIW3SyRcGmQLKSHehHS-XIVdSkODs";
-  static const String _baseUrl = "https://api.themoviedb.org/3";
+  static const String _baseUrl = "https://api.themoviedb.org";
+  static const String _apiVersion = "3";
 
   static Future<List<Movie>> fetchMovies(String query) async {
+    // TODO : use a middleware to set the headers
     final response = await http.get(
-      Uri.parse("$_baseUrl/search/movie?language=fr-FR&query=$query"),
+      Uri.parse("$_baseUrl/$_apiVersion/search/movie?language=fr-FR&query=$query"),
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
